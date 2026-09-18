242cde5d3bf0c2c4e30aeb23a88d4be447220871
Kyle Klus <kyle.klus.2@gmail.com>
2026-09-18 17:58:23 +0200

    FEAT: Fully implemented versuch 02

--- Versuch02/vhdl/counter_demo.vhdl
+++ Versuch02/vhdl/counter_demo.vhdl
@@ -6,6 +6,7 @@ entity counter_demo is
 port(
 	clock : in std_ulogic;
 	key0_n : in std_ulogic;
+	switch0: in std_ulogic_vector(3 downto 0);
 	hex0_n : out std_ulogic_vector(6 downto 0);
 	hex1_n : out std_ulogic_vector(6 downto 0)
 );
@@ -13,7 +14,9 @@ end counter_demo;
 
 architecture rtl of counter_demo is
 -- Signals, Constants, Components
+	constant base_frequency : std_ulogic_vector(25 downto 0) := "0000" & (21 downto 0 => '1');
 	signal not_key0_n, reduced_clk_wire: std_ulogic;
+	signal frequency_in : std_ulogic_vector(25 downto 0);
 	signal q_out_wire: std_ulogic_vector(3 downto 0);
 
 	component enableGen
@@ -49,7 +52,7 @@ not_key0_n <= not key0_n;
 enableGen_inst : enableGen
 port map(
 	clk => clock,
-	resetValue_in => std_ulogic_vector(to_unsigned(50000000, 26)),
+	resetValue_in => frequency_in,
 	nReset => key0_n,
 	clkEnable_out => reduced_clk_wire
 );
@@ -69,5 +72,10 @@ port map(
 	hex1_n => hex1_n
 );
 
+process(switch0)
+begin
+	frequency_in <= switch0 & base_frequency( 21 downto 0);
+
+end process;
 
 end rtl;
